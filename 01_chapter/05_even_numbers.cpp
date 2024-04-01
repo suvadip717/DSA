bff8b4220b2cf1ec19bf5bc53b1210cce8077113
suvadip717 <suvadipdas717@gmail.com>
2024-04-01 07:43:51 +0530

    different method

--- 01_chapter/05_even_numbers.cpp
+++ 01_chapter/05_even_numbers.cpp
@@ -17,3 +17,21 @@ int main()
     cout << endl;
     return 0;
 }
+
+//************************************** Adding 2 ********************************
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     for (int i = 2; i <= num; i = i + 2)
+//     {
+//         cout << i << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
