d2bd5b20d7a7efc019d3a3fefb4bf898082dd8c4
suvadip717 <suvadipdas717@gmail.com>
2024-04-01 07:34:41 +0530

    even numbers

---
+++ 01_chapter/05_even_numbers.cpp
@@ -0,0 +1,19 @@
+// ********************************** Print even numbers 1 to n ************************
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+    for (int i = 1; i <= num; i++)
+    {
+        if (i % 2 == 0)
+        {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
