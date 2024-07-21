258c4ad46c9186bb4c1dabd0031f2ee895af27b8
suvadip717 <suvadipdas717@gmail.com>
2024-07-21 23:54:38 +0530

    sqrt(x) leetcode

---
+++ 05_chapter/17_SqureRoot.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+  int n, ans;
+  cout << "Enter a number for get squre root of the number: ";
+  cin >> n;
+  ans = sqrt(n);
+  cout << "The number squre root is: " << ans << endl;
+  return 0;
+}
