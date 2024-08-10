b320ee9fefeda0518c34a1bf01a8c716649add25
Suvadip Das <suvadipdas717@gmail.com>
2024-08-10 23:09:19 +0530

    Unique number Coding Ninja

---
+++ 07_chapter/07_FindUnique.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, t, arr[100];
+    cout<<"Enter number of test cases: ";
+    cin>>n;
+    for(int i =0; i<n; i++){
+      cout<<"Enter size of array: ";
+      cin>>t;
+      int res=0;
+      for(int j=0; j<t; j++){
+        cin>>arr[j];
+        res^=arr[j];
+      }
+      cout<<res<<endl;
+    }
+    return 0;
+}
